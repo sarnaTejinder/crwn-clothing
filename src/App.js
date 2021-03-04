9b5c2817d4236ba06d594c87742924ab14c11113
sarnaTejinder <sarnaTejinder@users.noreply.github.com>
2021-03-04 23:53:36 +0530

    made components , finished styling menu-item component

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './App.css';
+
+import HomePage from './pages/homepage/homepage.component'
+
+class App extends React.Component {
+
+
+  render() {
+
+    return (
+      <div className="App">
+        <HomePage />
+      </div>
+    );
+  }
+}
+
+export default App;
